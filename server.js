7f8e2830aaede32037d7cd31e25640564dcd5efe
Ulan <ulanr92@gmail.com>
2024-03-22 14:56:07 -0500

    connect editor route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import studentsRouter from "./routes/students/index.js";
 import instructorsRouter from "./routes/instructors/index.js";
+import editorRouter from "./routes/editor/index.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./openapi.json" assert { type: "json" };
 import cors from "cors";

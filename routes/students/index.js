de1a539bf5bb6609f6b1700794b3a942d1450dff
Ulan <ulanr92@gmail.com>
2024-07-11 16:41:56 -0500

    tgm-100

--- routes/students/index.js
+++ routes/students/index.js
@@ -56,9 +56,10 @@ router.get("/:id", async (req, res) => {
       WHERE STUDENTS.STUDENT_ID = :id`,
       [id]
     );
-    // 404
     const rows = result?.rows;
 
+    console.log("result", result);
+    // 404
     if (rows?.length <= 0) {
       return res.status(404).send(`Student not found with this id of ${id}`);
     }
@@ -195,6 +196,208 @@ router.put("/:id", async (req, res) => {
 
     return res.status(200).send(`Successfully updated ${FIRST_NAME}`);
   } catch (err) {
+    console.error("Error", err.message);
+
+    if (err.errorNum === 2004) {
+      return res.status(409).send({
+        message: "The email you have entered is already in use!",
+      });
+    } else {
+      return res.status(500).send({
+        message: "Error updating student to DB",
+      });
+    }
+  } finally {
+    if (connection) {
+      // Release connection
+      try {
+        await connection.release();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+});
+
+/**
+ * PATCH /:id
+ * Update a student partially
+ */
+router.patch("/:id", async (req, res) => {
+  let connection;
+  try {
+    connection = await dbSingleton.createConnection();
+    const { id } = req.params;
+    const parseId = parseInt(id);
+
+    if (isNaN(parseId)) {
+      return res.status(400).send("Incorrect id provided");
+    }
+
+    if (parseId === 1 || parseId === 2) {
+      return res
+        .status(403)
+        .send("Not Authorized to update first 2 default students");
+    }
+
+    const result = await connection.execute(
+      `
+      SELECT STUDENTS.*, INSTRUCTORS.FULLNAME AS INSTRUCTOR_NAME
+	    FROM STUDENTS
+	    JOIN INSTRUCTORS
+	    ON STUDENTS.INSTRUCTOR_ID = INSTRUCTORS.INSTRUCTOR_ID
+      WHERE STUDENTS.STUDENT_ID = :id`,
+      [id]
+    );
+    // console.log("result", result);
+
+    const rows = result?.rows;
+
+    console.log("rows", rows);
+
+    // 404
+    if (rows?.length <= 0) {
+      return res.status(404).send(`Student not found with this id of ${id}`);
+    }
+
+    const student = rows[0];
+
+    // TODO: Needs helper function to minimize the code size
+
+    // First Name
+    if (req.body.FIRST_NAME) {
+      if (student.FIRST_NAME === req.body.FIRST_NAME) {
+        return res.status(304).send("Nothing has changed");
+      }
+      student.FIRST_NAME = req.body.FIRST_NAME;
+
+      const result = await connection.execute(
+        `UPDATE STUDENTS
+        SET FIRST_NAME=:firstName 
+        WHERE STUDENT_ID=:id`,
+        [req.body.FIRST_NAME, id],
+        { autoCommit: true }
+      );
+
+      if (!result || result.rowsAffected === 0) {
+        throw new Error("Error updating student to DB");
+      }
+
+      return res.status(200).json({
+        student: student,
+        message: `Student's first name updated to '${req.body.FIRST_NAME}'`,
+      });
+    }
+
+    // Last Name
+    if (req.body.LAST_NAME) {
+      if (student.LAST_NAME === req.body.LAST_NAME) {
+        return res.status(304).send("Nothing has changed");
+      }
+      student.LAST_NAME = req.body.LAST_NAME;
+
+      const result = await connection.execute(
+        `UPDATE STUDENTS
+        SET LAST_NAME=:lastName 
+        WHERE STUDENT_ID=:id`,
+        [req.body.LAST_NAME, id],
+        { autoCommit: true }
+      );
+
+      if (!result || result.rowsAffected === 0) {
+        throw new Error("Error updating student to DB");
+      }
+
+      return res.status(200).json({
+        student: student,
+        message: `Student's last name updated to '${req.body.LAST_NAME}'`,
+      });
+    }
+
+    // Email -> error
+    if (req.body.EMAIL) {
+      if (student.EMAIL === req.body.EMAIL) {
+        return res.status(304).send("Nothing has changed");
+      }
+      student.EMAIL = req.body.EMAIL;
+
+      const result = await connection.execute(
+        `UPDATE STUDENTS
+        SET EMAIL=:email 
+        WHERE STUDENT_ID=:id`,
+        [req.body.EMAIL, id],
+        { autoCommit: true }
+      );
+
+      if (!result || result.rowsAffected === 0) {
+        throw new Error("Error updating student to DB");
+      }
+
+      return res.status(200).json({
+        student: student,
+        message: `Student's email updated to '${req.body.EMAIL}'`,
+      });
+    }
+
+    // DOB
+    if (req.body.DOB) {
+      if (student.DOB === req.body.DOB) {
+        return res.status(304).send("Nothing has changed");
+      }
+      student.DOB = req.body.DOB;
+
+      const result = await connection.execute(
+        `UPDATE STUDENTS
+        SET DOB=TO_DATE(substr(:dob, 1, 10),'YYYY-MM-DD') 
+        WHERE STUDENT_ID=:id`,
+        [req.body.DOB, id],
+        { autoCommit: true }
+      );
+
+      if (!result || result.rowsAffected === 0) {
+        throw new Error("Error updating student to DB");
+      }
+
+      return res.status(200).json({
+        student: student,
+        message: `Student's dob updated to '${req.body.DOB}'`,
+      });
+    }
+
+    // INSTRUCTOR_ID
+    if (req.body.INSTRUCTOR_ID) {
+      if (![1, 2, 3, 4].includes(req.body.INSTRUCTOR_ID)) {
+        return res
+          .status(400)
+          .send("Wrong Instructor ID, It can be 1,2,3 or 4.");
+      }
+
+      if (student.INSTRUCTOR_ID === req.body.INSTRUCTOR_ID) {
+        return res.status(304).send("Nothing has changed");
+      }
+      student.INSTRUCTOR_ID = req.body.INSTRUCTOR_ID;
+
+      const result = await connection.execute(
+        `UPDATE STUDENTS
+        SET INSTRUCTOR_ID=:instructorId 
+        WHERE STUDENT_ID=:id`,
+        [req.body.INSTRUCTOR_ID, id],
+        { autoCommit: true }
+      );
+
+      if (!result || result.rowsAffected === 0) {
+        throw new Error("Error updating student to DB");
+      }
+
+      return res.status(200).json({
+        student: student,
+        message: `Student's instructor id updated to '${req.body.INSTRUCTOR_ID}'`,
+      });
+    }
+
+    // return ???
+  } catch (err) {
+    // return ???
     console.error("Error", err.message);
 
     if (err.errorNum === 2004) {
